d4e7f699a6275b662ccb5eed928b91727214a2ec
Dzoel31 <dzulfikriadjmal@gmail.com>
2023-05-03 13:51:21 +0700

    Menambah data diri

--- CobaGit.py
+++ CobaGit.py
@@ -1,3 +1,3 @@
 print("Selamat datang pada git dan github!")
 print("Ini merupakan commit pertama pada file ini!")
-print("Halo nama saya Dzulfikri Adjmal")
+print("Halo nama saya Dzulfikri Adjmal lahir di Jakarta")
