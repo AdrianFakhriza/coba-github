1e5cbfc76355d9514ba9ecd607c5f35c6a799309
Dzoel31 <dzulfikriadjmal@gmail.com>
2023-05-05 09:52:17 +0700

    Menambah baris ke-4

--- CobaGit.py
+++ CobaGit.py
@@ -1,3 +1,4 @@
 print("Selamat datang pada git dan github!")
 print("Ini merupakan commit pertama pada file ini!")
 print("Halo nama saya Dzulfikri Adjmal lahir di Jakarta")
+print("Halooo semuanya selamat pagi!")
